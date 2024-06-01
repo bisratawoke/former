c8e48511aba7fa17a4624256a448d8b512746d61
bisratawoke <bisratawoke52@gmail.com>
2024-06-01 16:49:11 +0300

    Subject Line: Added google auth configuration to next auth config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth/next";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
     Github({
       clientId: String(process.env.GITHUB_CLIENT_ID),
       clientSecret: String(process.env.GITHUB_SECRET),
+    }),
+    Google({
+      clientId: String(process.env.GOOGLE_CLIENT_ID),
+      clientSecret: String(process.env.GOOGLE_SECRET),
     }),
   ],
 };
